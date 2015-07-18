5b313c2b9f461ae8c705b0667890d99d56813a9c
Michael Fenn <fennm@mailbox.sc.edu>
2015-07-18 17:54:32 +0100

    Pre-calculate aggregates on topLevelFolder and vcenter for effiicency

--- bin/vsphere-influxdb-import.py
+++ bin/vsphere-influxdb-import.py
@@ -9,6 +9,7 @@ import ssl
 
 from pyVim import connect
 from pyVmomi import vim
+from pyVsphereInflux import InfluxResult08
 from pyVsphereInflux.vsphere import build_vmresultset
 from pyVsphereInflux.influx import write_results
 from pyVsphereInflux.tools.regex import convert_to_alnum
@@ -32,6 +33,88 @@ def silence_warnings():
     else:
         # Handle target environment that doesn't support HTTPS verification
         ssl._create_default_https_context = _create_unverified_https_context
+
+def agg_by_vcenter(results):
+    # agg_toplevelfolder is dictionary whose values are
+    # InfluxResults
+    # agg_vcenter['vcenter']
+    agg_vcenter = {}
+    meas_base = "vmagg_vcenter"
+    for ts in results:
+        vcenter = convert_to_alnum(ts.tags['vcenter'])
+
+        # new vcenter, which means new aggregrate
+        if vcenter not in agg_vcenter:
+            agg_obj = InfluxResult08("%s.%s" % (meas_base, vcenter))
+            for tag in ['vcenter']:
+                agg_obj.tags[tag] = ts.tags[tag]
+            for field in ts.fields:
+                agg_obj.fields[field] = ts.fields[field]
+            agg_obj.timestamp = ts.timestamp
+            agg_vcenter[vcenter] = agg_obj
+        else:
+            agg_obj = agg_vcenter[vcenter]
+            # aggregate on fields
+            for field in ts.fields:
+                if field not in agg_obj.fields:
+                    agg_obj.fields[field] = ts.fields[field]
+                elif type(agg_obj.fields[field]) in (int, long, float):
+                    agg_obj.fields[field] += ts.fields[field]
+
+    # now flatten to a list
+    ret = []
+    for vcenter in agg_vcenter:
+        ret.append(agg_vcenter[vcenter])
+
+    return ret
+
+def agg_by_topLevelFolder(results):
+    # agg_toplevelfolder is a set of nested dictionaries whose values are
+    # InfluxResults
+    # agg_toplevelfolder['vcenter']['datacenter']['afolder']
+    agg_topLevelFolder = {}
+    meas_base = "vmagg_topLevelFolder"
+    for ts in results:
+        vcenter = convert_to_alnum(ts.tags['vcenter'])
+        datacenter = convert_to_alnum(ts.tags['datacenter'])
+        topLevelFolder = convert_to_alnum(ts.tags['topLevelFolder'])
+
+        # new vcenter
+        if vcenter not in agg_topLevelFolder:
+            agg_topLevelFolder[vcenter] = {}
+
+        # new datacenter 
+        if datacenter not in agg_topLevelFolder[vcenter]:
+            agg_topLevelFolder[vcenter][datacenter] = {}
+
+        # new topLevelFolder, which means new aggregrate
+        if topLevelFolder not in agg_topLevelFolder[vcenter][datacenter]:
+            agg_obj = InfluxResult08("%s.%s.%s.%s" % \
+                (meas_base, vcenter, datacenter, topLevelFolder))
+            for tag in ['vcenter', 'datacenter', 'topLevelFolder']:
+                agg_obj.tags[tag] = ts.tags[tag]
+            for field in ts.fields:
+                agg_obj.fields[field] = ts.fields[field]
+            agg_obj.timestamp = ts.timestamp
+            agg_topLevelFolder[vcenter][datacenter][topLevelFolder] = agg_obj
+        else:
+            agg_obj = agg_topLevelFolder[vcenter][datacenter][topLevelFolder] 
+            # aggregate on fields
+            for field in ts.fields:
+                if field not in agg_obj.fields:
+                    agg_obj.fields[field] = ts.fields[field]
+                elif type(agg_obj.fields[field]) in (int, long, float):
+                    agg_obj.fields[field] += ts.fields[field]
+
+    # now flatten to a list
+    ret = []
+    for vcenter in agg_topLevelFolder:
+        for datacenter in agg_topLevelFolder[vcenter]:
+            for topLevelFolder in agg_topLevelFolder[vcenter][datacenter]:
+                ret.append(
+                    agg_topLevelFolder[vcenter][datacenter][topLevelFolder])
+
+    return ret
 
 def main():
     # take some input 
@@ -77,16 +160,23 @@ def main():
                 ts.tags['topLevelFolder'] = ts.tags['folderPath'].split('/')[1]
             else:
                 ts.tags['topLevelFolder'] = "None"
-        
+
+        # collect the results and some aggregates 
+        output = []
+        output.extend(results)
+        output.extend(agg_by_topLevelFolder(results))
+        output.extend(agg_by_vcenter(results))
+
+        # now write the output to the database
         if args.debug:
             print "Results of vSphere query:"
-            for ts in results:
+            for ts in output:
                 print "Measurement:", ts.measurement
                 print "Tags:", ts.tags
                 print "Fields:", ts.fields
                 print
         else:
-            write_results(args.influx_dsn, results)
+            write_results(args.influx_dsn, output)
 
 if __name__ == '__main__':
     sys.exit(main())
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from glob import glob
 
 setup(name='pyVsphereInflux',
-      version='0.2',
+      version='0.3',
       description='A library and supporting script for pulling data from vSphere and inserting it into InfluxDB',
       scripts=glob('bin/*'),
       package_dir={'': 'lib'},
